812c8a75124e73267a5128ff8c8b346677b6c200
moka <moka.drip.web@gmail.com>
2021-09-19 22:34:37 +0900

    Initial commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+# pim.hello.py
+# test.hello.py
+
+def hello():
+    num = 1
+    print(num)
+
+hello()
---
+++ main.py
@@ -0,0 +1,26 @@
+# Create.main.py
+# pim.main.py
+
+import wx
+
+
+def clickSaveButton():
+    saveFile = open(txtName.GetValue(), 'w')
+    saveFile.write(text.GetValue())
+    saveFile.close()
+
+
+if __name__ == '__main__':  # Processing from this file.
+    window = wx.App()
+    frame = wx.Frame(None, -1, 'pim', size=(870, 700))
+
+    panel = wx.Panel(frame, -1)  # createPanel
+    saveButton = wx.Button(panel, -1, 'Save', pos=(120, 10))
+    txtName = wx.TextCtrl(panel, -1, pos=(10, 10))
+    text = wx.TextCtrl(panel, -1, pos=(10, 50), size=(850, 610), style=wx.TE_MULTILINE)
+    font = wx.Font(20, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)  # fontSetting.
+    text.SetFont(font)
+    saveButton.Bind(wx.EVT_BUTTON, clickSaveButton)
+
+    frame.Show()
+    window.MainLoop()
